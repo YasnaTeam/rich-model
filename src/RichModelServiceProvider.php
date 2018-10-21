6fc38571441c282a196e9785e8e239a9383432e8
Taha Kamkar <chieftaha@gmail.com>
2018-10-21 09:14:33 +0330

    make a fresh bed

---
+++ src/RichModelServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Yasnateam\Modular;
+
+use Illuminate\Support\ServiceProvider;
+
+class RichModelServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
